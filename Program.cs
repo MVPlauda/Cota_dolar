e622be646558ba2a219a3d3b36c9fab07a73eeef
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:51:42 -0300

    Cota_dolar

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Construa um algoritmo que leia a cotação do dólar, 
+// leia um valor em dólares, 
+// converta esse valor para Real e mostre o resultado.
+
+
+
+internal class program
+{
+    private static void Main()
+    {
+        float dolar, real, cotação;
+
+        Console.Write("Insira a cotação atual: ");
+        cotação = float.Parse(Console.ReadLine());
+        Console.Write("Insira um valor para ser transformado: ");
+        dolar = float.Parse(Console.ReadLine());
+
+        real = dolar * cotação;
+
+        Console.WriteLine($"O valor transformado em real é: {real}");
+    }
+}
+
